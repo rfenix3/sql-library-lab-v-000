1ed8409f391e551628dc266b0cc796902d5b5bb8
Richard Fenix <richard.fenix@gmail.com>
2018-07-21 03:51:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,29 @@
 INSERT into authors (name)
-VALUES ("George Lucas"), ("Rowling");
+VALUES ("George Lucas"), ("J. K. Rowling");
+
+INSERT into subgenre (name)
+VALUES ("Sci-Fi"),("Fantasy");
 
 INSERT into series (title, author_id, subgenre_id)
-VALUES ("Harry Potter", 2, 2);
+VALUES ("Harry Potter", 2, 2), ("Star Wars", 1, 1);
 
-INSERT into series (title, author_id, subgenre_id)
-VALUES ("Harry Potter", 2, 2);
- 
+INSERT into books (title, year, series_id)
+VALUES 
+("A New Hope", 1979, 2), 
+("Empire Striks Back", 1980, 2),
+("Return of the Jedi", 1982, 2),
+("Philosopher's Stone", 1997, 1),
+("Chamber of Secrets", 1998, 1),
+("Prisoner of Azkaban", 1999, 1);
+
+INSERT into characters (name, species, motto, series_id, author_id)
+VALUES 
+("Han Solo", "Human", "Mambo Jumbo", 2, 1), 
+("Luke Skywalker", "Human", "Nooo! You are not my father!", 2, 1), 
+("Obi-wan", "Human", "He is ready", 2, 1), 
+("Wicket", "Ewok", "Trrii Wok Wok Wok", 2, 1), 
+("Harry", "Human", "Trrii Wok Wok Wok", 2, 1), 
+("Hermione", "Human", "Trrii Wok Wok Wok", 2, 1), 
+("Peter Pettigrew", "Rodent", "Trrii Wok Wok Wok", 2, 1), 
+("Sirious Black", "Werewolf", "Trrii Wok Wok Wok", 2, 1);
+
