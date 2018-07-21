fcc0330556d5769a89a4910a14e23cb0e6d4b35d
Richard Fenix <richard.fenix@gmail.com>
2018-07-21 04:18:55 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year from "
+  "SELECT title, year from books where series_id = 1 ORDER by year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
