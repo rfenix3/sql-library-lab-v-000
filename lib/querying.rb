f2f6ef06e0c82b20236c02035e9247f7d4ae315a
Richard Fenix <richard.fenix@gmail.com>
2018-07-21 05:21:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,11 +22,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Select series_id, count(species) count
+  "Select series_id from characters
+  where count(species) = (select count(species) 
   from characters 
   WHERE characters.species = 'human'
   GROUP BY series_id
-  ORDER by series_id LIMIT 1"
+  ORDER by series_id LIMIT 1)"
 
   # "Select series.title title, count(characters.species) count
   # from series
