76d4c994b4981a58331c8e48a13db5190f75126f
Richard Fenix <richard.fenix@gmail.com>
2018-07-21 03:41:05 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,9 @@
-INSERT into series ()
+INSERT into authors (name)
+VALUES ("George Lucas"), ("Rowling");
+
+INSERT into series (title, author_id, subgenre_id)
+VALUES ("Harry Potter", 2, 2);
+
+INSERT into series (title, author_id, subgenre_id)
+VALUES ("Harry Potter", 2, 2);
+ 
