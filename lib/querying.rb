c72e564304d20b1489f8a5e29485f0d79160584d
Richard Fenix <richard.fenix@gmail.com>
2018-07-21 05:38:14 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,8 +38,8 @@ def select_series_title_with_most_human_characters
   from series
   INNER JOIN characters ON series.id = characters.series_id
   WHERE characters.species = 'human'
-  GROUP BY title
-  HAVING count(characters.species) = MAX(characters.species)"
+  GROUP BY title"
+  # HAVING count(characters.species) = MAX(characters.species)"
 
 end
 
